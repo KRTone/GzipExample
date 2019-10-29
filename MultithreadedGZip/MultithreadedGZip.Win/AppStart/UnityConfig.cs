782966afc888ca505af5c3683ace68117e675fc1
rkudaybergenov <rkudaybergenov@baltbet.ru>
2019-10-29 12:27:47 +0300

    fix ExceptionHandler

--- MultithreadedGZip/MultithreadedGZip.Win/AppStart/UnityConfig.cs
+++ MultithreadedGZip/MultithreadedGZip.Win/AppStart/UnityConfig.cs
@@ -15,13 +15,12 @@ namespace MultithreadedGZip.Win.AppStart
             AppDomain.CurrentDomain.UnhandledException += (s, e) =>
             {
                 var ex = e.ExceptionObject as Exception;
-                Exception exToLog;
-                do
+                Exception exToLog = ex;
+                while (exToLog.InnerException != null)
                 {
                     exToLog = ex.InnerException;
                 }
-                while (exToLog.InnerException != null);
-
+                
                 container.Resolve<BLL.Interfaces.ILogService>().Exception(exToLog);
 
                 Console.WriteLine("Program failed");
@@ -32,13 +31,8 @@ namespace MultithreadedGZip.Win.AppStart
             };
 
             Console.WriteLine("Register types ...");
-            RegisterTypes(container, args);
-            return container;
-        }
-
-        static void RegisterTypes(IUnityContainer container, string[] args)
-        {
             CompositionRoot.UnityConfig.RegisterTypes(container, args);
+            return container;
         }
     }
 }
